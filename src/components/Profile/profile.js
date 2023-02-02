d49208a57900b84fe4b2bc52af1f75df45780c41
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-02 04:21:43 +0200

    profile component

---
+++ src/components/Profile/profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export function Profile(props) {
+    return (
+      <div class="profile">
+    <div class="description">
+      <img
+        src={props.avatar}
+        alt="User avatar"
+        class="avatar"
+      />
+      <p class="name">{props.username}</p>
+      <p class="tag">{props.tag}</p>
+      <p class="location">{props.location}</p>
+    </div>
+  
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">{props.stats.followers}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{props.stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{props.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+    );
+  };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -8,3 +8,25 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+/* 
+import { createRoot } from 'react-dom/client';
+createRoot(document.querySelector('#root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); 
+*/
+
+/* import { createRoot } from 'react-dom/client';
+const root = createRoot(document.querySelector('#root'));
+
+const el = (
+  <div>
+    <div>Ура</div>
+    <div>Ура</div>
+    <div>Ура</div>
+  </div>
+);
+
+root.render(el); */
