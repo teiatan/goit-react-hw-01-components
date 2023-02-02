326984896c9dea62cf7a6b2e2ac698923a5e28ad
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-02 04:32:03 +0200

    destructurisation

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,31 +1,34 @@
 import React from 'react';
 
 export function Profile(props) {
+
+    const { username, tag, location, avatar, stats} = props;
+
     return (
       <div class="profile">
     <div class="description">
       <img
-        src={props.avatar}
+        src={avatar}
         alt="User avatar"
         class="avatar"
       />
-      <p class="name">{props.username}</p>
-      <p class="tag">{props.tag}</p>
-      <p class="location">{props.location}</p>
+      <p class="name">{username}</p>
+      <p class="tag">{tag}</p>
+      <p class="location">{location}</p>
     </div>
   
     <ul class="stats">
       <li>
         <span class="label">Followers</span>
-        <span class="quantity">{props.stats.followers}</span>
+        <span class="quantity">{stats.followers}</span>
       </li>
       <li>
         <span class="label">Views</span>
-        <span class="quantity">{props.stats.views}</span>
+        <span class="quantity">{stats.views}</span>
       </li>
       <li>
         <span class="label">Likes</span>
-        <span class="quantity">{props.stats.likes}</span>
+        <span class="quantity">{stats.likes}</span>
       </li>
     </ul>
   </div>
